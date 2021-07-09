15c1f5ba18f0df8aae45bf620f61329e0b30f9f8
Anton1 <loukik56@mail.ru>
2021-07-09 09:40:04 +0300

    Added files README.md and setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Tasc_6 Report of Monaco 2018 Racing',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='ThinkPad',
+    author_email='Loukik56@mail.ru',
+    description=''
+)
